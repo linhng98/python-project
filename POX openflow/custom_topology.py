f964e7c84e506e3bce3a82dd34ce821a3870e3d0
nobabykill <linh1612340@gmail.com>
2020-06-03 09:33:08 +0700

    init topology

---
+++ POX openflow/custom_topology.py
@@ -0,0 +1,50 @@
+"""Custom topology example
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+
+class MyTopo(Topo):
+    "Simple topology example."
+
+    def __init__(self):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__(self)
+
+        # Add hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+
+        # Add switchs
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+
+        # Add links switch to host
+        self.addLink(s1, h1)
+        self.addLink(s2, h2)
+        self.addLink(s3, h3)
+        self.addLink(s4, h4)
+        self.addLink(s5, h5)
+
+        # Add links switch to switch
+        self.addLink(s1, s2)
+        self.addLink(s1, s3)
+        self.addLink(s1, s4)
+        self.addLink(s2, s4)
+        self.addLink(s2, s5)
+        self.addLink(s3, s4)
+        self.addLink(s4, s5)
+
+
+topos = {'mytopo': (lambda: MyTopo())}
